df1b172e3e86a0ff273fdfedeb470bedf9106c6b
atmosfar <atmosfar@doublejump.eu>
2026-03-29 14:34:43 +0100

    Added ProjectParser and ProjectWriter base classes

---
+++ include/common/ProjectWriter.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2026 atmosfar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PROJECT_WRITER_H
+#define PROJECT_WRITER_H
+
+#include <string>
+#include "ProjectInfo.h"
+
+namespace reaconv {
+
+class ProjectWriter {
+public:
+    ProjectWriter();
+    virtual ~ProjectWriter();
+
+    virtual bool Write(const ProjectInfo& project, const std::string& filePath) = 0;
+};
+
+} // namespace reaconv
+
+#endif // PROJECT_WRITER_H
